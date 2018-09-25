5edfcd43b4c6f17b63106f3eb8771ef88013e646
Keegan Crankshaw <kcranky@gmail.com>
2018-09-25 21:50:12 +0200

    Seperate file to store paramter definitions

---
+++ parameterDefinitions.py
@@ -0,0 +1,13 @@
+Word32 = {
+   "WORD_MSB": 31,
+   "EXPONENT_MSB" : 7,
+   "MANTISSA_MSB" : 22,
+   "EXPONENT_BIAS" : (pow(2,8)/2-1)
+}
+
+Word16 = {
+   "WORD_MSB": 15,
+   "EXPONENT_MSB" : 4,
+   "MANTISSA_MSB" : 9,
+   "EXPONENT_BIAS" : (pow(2,5)/2-1)
+}
