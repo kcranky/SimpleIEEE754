a8fe89220d9297140dd32417b4295af3669f7bae
Keegan Crankshaw <kcranky@gmail.com>
2018-09-25 22:56:39 +0200

    Negative numbers added

--- packUnpack.py
+++ packUnpack.py
@@ -2,12 +2,6 @@
 from struct import *
 from parameterDefinitions import Word32, Word16
 
-def twos_comp(val, bits):
-   val = int(val,2)
-   if (val & (1 << (bits - 1))) != 0: # if sign bit is set e.g., 8bit: 128-255
-      val = val - (1 << bits)        # compute negative value
-   return val                         # return positive value as is
-
 def parse_bin_frac(s):
    return int(s[1:], 2) / 2.**(len(s) - 1)
 
@@ -20,7 +14,7 @@ def get_dec_value(binVal, struct):
    s = int(sign, 2)
    e = int(exponent, 2)
    m = parse_bin_frac("." + mantissa)
-   #print(bin(s), bin(e), bin(man))
+   #print(s, e, m)
    result = pow(-1, s) * (1+m) * pow(2, e-struct["EXPONENT_BIAS"])  
    print("{} converted to   {}").format(binVal, result)
    return result
@@ -38,6 +32,7 @@ def get_bin_value(decVal, struct):
    for j in range(struct["MANTISSA_MSB"]):
       frac = float(frac*2)
       t = ("{0:f}").format(float(frac)).split(".")
+      print(t, f)
       f = str(f) + t[0]
       #print(f, frac)
       frac = float(t[1])/pow(10,len(t[1]))
@@ -45,20 +40,22 @@ def get_bin_value(decVal, struct):
          break
    
    #normalise
-   #print("i={}").format(i)
-   #print("f={}").format(f)
+   print("i={}").format(i)
+   print("f={}").format(f)
    mantissa = i+f
    c = 0
+   
    while mantissa[0] != "1":
       mantissa=mantissa[1:]
       c = c-1
-   
+       
    #bias the exponent
    if (c != 0):
-      #print("expc={}").format(c + struct["EXPONENT_BIAS"])
-      exp = bin(c + struct["EXPONENT_BIAS"])
-      exp = str(str(exp)[2:]).rjust(struct["EXPONENT_MSB"]+1,"0")
-      #mantissa = mantissa[1:]
+         print("expc={}").format(c + struct["EXPONENT_BIAS"])
+         exp = bin(c + struct["EXPONENT_BIAS"])
+         exp = str(str(exp)[2:]).rjust(struct["EXPONENT_MSB"]+1,"0")
+         print("expc={}").format(exp)
+
    else:
       exp = bin(len(i)-1+struct["EXPONENT_BIAS"])[2:]
       #print("exp={}").format(exp)
@@ -66,8 +63,7 @@ def get_bin_value(decVal, struct):
    #print("mantissa=" +mantissa)
    mantissa = str(str(mantissa)[1:]).ljust(struct["MANTISSA_MSB"]+1,"0")
    
-   
-   if (decVal > 0):
+   if (float(decVal) > 0):
       result = ("0"+exp+mantissa)[0:struct["WORD_MSB"]+1]
    else:
       result = ("1"+exp+mantissa)[0:struct["WORD_MSB"]+1]
@@ -77,9 +73,11 @@ def get_bin_value(decVal, struct):
    
   
 if __name__ == "__main__":
-   get_dec_value(get_bin_value("0.375", Word32), Word32) 
-   get_dec_value(get_bin_value("0.25", Word32), Word32)
-   get_dec_value(get_bin_value("99999514624.0", Word32), Word32) 
-   get_dec_value(get_bin_value("0.99999514624", Word32), Word32)
+   get_bin_value("-0.235", Word32)
+   get_dec_value("10111110011100001010001111010111", Word32)   
+   
+   get_bin_value("-0.09375", Word16)
+   get_dec_value("1010111000000000", Word16)   
+   
    print("Finished")
    
